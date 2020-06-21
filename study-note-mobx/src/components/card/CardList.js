e97ee0b7b8a833b27cf9e77883635d10db3e856b
blacktokki <39031723+blacktokki@users.noreply.github.com>
2020-06-21 22:32:03 +0900

    저장상태 추가

--- study-note-mobx/src/components/card/CardList.js
+++ study-note-mobx/src/components/card/CardList.js
@@ -11,43 +11,12 @@ const Results = SortableContainer((props) => {
   )
 });
 
-export default inject("card","router")(
-  observer(({index, card, router})=>{
-    if (index=== 'new'){
-      if (card.id !== undefined){
-        card.id = undefined
-        card.handleTitle('제목')
-        card.cards=[]
-      }
-    }
-    else if(index < router.root.note.notes.length){
-      if (card.id !== index){
-        card.id = index
-        card.handleTitle(card.root.note.notes[index].title)
-        card.cards=JSON.parse(JSON.stringify(card.root.note.notes[index].cards))
-      }
-    }
-    else{
-      router.push('/');
-    }
-
+export default inject("card")(
+  observer(({card})=>{
     const onSortEnd = ({oldIndex, newIndex}) => {
       card.replaceCard(oldIndex, newIndex)
     };
-    const save = () => {
-      if (card.id===undefined){
-        card.root.note.addNote(card)
-        router.push('/note/'+card.id);
-      }
-      else
-        card.root.note.handleNote(card.id, card) 
-    }
-    useEffect(() => {
-      if(card.is_scroll){
-        window.scrollTo(0,document.body.scrollHeight);
-        card.is_scroll = false
-      }
-    });
+    
     const results = card.cards.map(
       (item,idx) => (
         <CardNew 
@@ -62,13 +31,19 @@ export default inject("card","router")(
       )
     )
 
+    useEffect(() => {
+      if(card.is_scroll){
+        window.scrollTo(0,document.body.scrollHeight);
+        card.is_scroll = false
+      }
+    });
+
     return (
     <div data-changed={card.changed}>
       <Results onSortEnd={onSortEnd}>
         {results}
       </Results>
       <button onClick={(event) => {card.addCard(card.cards.length);}}>+++</button>
-      <button onClick={save}>저장</button>
     </div>
     );
   }
--- study-note-mobx/src/store/router.js
+++ study-note-mobx/src/store/router.js
@@ -1,4 +1,8 @@
 import { RouterStore as _RouterStore } from 'mobx-react-router';
 
 export default class RouterStore extends _RouterStore{
+    constructor(root){
+        super()
+        this.root = root
+    }
 }
