87201fe5b4568a62d44cc3dcf9b37b0e81a2aad0
blacktokki <39031723+blacktokki@users.noreply.github.com>
2020-06-28 16:01:49 +0900

    rn 세팅1

--- study-note-rn/App.js
+++ study-note-rn/App.js
@@ -1,14 +1,32 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform,View } from 'react-native';
 import { Header, Main,Left,Footer } from 'layout';
-import { createBrowserHistory } from 'history';
+import { createMemoryHistory } from 'history';
 import { Provider } from 'mobx-react';
 import { Router } from 'react-router-dom';
 import { syncHistoryWithStore } from 'mobx-react-router';
 import Store from 'store/index';
+
+//import "public/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.css";
+import "public/css/style.css";
+import "public/css/components.css";
+//import '@fortawesome/fontawesome-free/css/all.css';
+
+import jQuery  from  "jquery";
+//import "bootstrap";
+//import "jquery.nicescroll";
+window.$ = jQuery
+window.jQuery = jQuery
+//require("public/js/moment.min.js");
+//require("public/js/popper.min.js");
+//require("public/js/scripts.js");
+//require("public/js/stisla.js");
+
+
 const store = new Store();
-const browserHistory = createBrowserHistory();
-const history = syncHistoryWithStore(browserHistory,store.router)
+const memoryHistory =  createMemoryHistory();
+const history = syncHistoryWithStore(memoryHistory,store.router)
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -19,30 +37,13 @@ export default function App() {
   return (
     <Provider {...store}>
       <Router className="App" history={history}>
-        <Header></Header>
-        <Left></Left>
-        <Main></Main>
-        <Footer></Footer>
+        <View>
+          <Header></Header>
+          <Left></Left>
+          <Main></Main>
+          <Footer></Footer>
+        </View>
       </Router>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- study-note-rn/metro.config.js
+++ study-note-rn/metro.config.js
@@ -1,5 +1,21 @@
-module.exports = {
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-css-transformer")
+    },
+    resolver: {
+      sourceExts: [...sourceExts, "css"]
+    }
+  };
+})();
+/*module.exports = {
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   },
 };
+*/
