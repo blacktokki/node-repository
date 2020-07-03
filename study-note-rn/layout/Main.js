0fa76947aee439504c31df744f1a8326b4bb5fe0
blacktokki <39031723+blacktokki@users.noreply.github.com>
2020-07-03 11:13:41 +0900

    rn router 적용2

--- study-note-rn/layout/Main.js
+++ study-note-rn/layout/Main.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Switch ,Route } from 'router/router';
+import { Route } from 'router/router';
 import {Home,Option,Card,Note,Study} from 'components';
 import { View } from 'react-native';
 
 function Main() {
   return (
     <View className="main-content">
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/option" component={Option}/>
-        <Route exact path="/note" component={Note}/>
-        <Route exact path="/note/:id" component={Card}/>
-        <Route path="/study" component={Study}/>
-      </Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/option" component={Option}/>
+      <Route exact path="/note" component={Note}/>
+      <Route exact path="/note/:id" component={Card}/>
+      <Route path="/study" component={Study}/>
     </View>
   );
 }
