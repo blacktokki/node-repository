e9c852a6668412a506cd42a93654bffb435437a7
blacktokki <39031723+blacktokki@users.noreply.github.com>
2020-08-20 23:03:43 +0900

    mobile dragable

--- study-note-rn/layout/Footer.js
+++ study-note-rn/layout/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { A, Div } from 'components/commons'
 function Footer() {
     return(
-        <Div className="main-footer">
+        <Div className="main-footer" style={{
+            paddingVertical : Platform.OS === 'web' ? 20 : 5,
+            paddingHorizontal : Platform.OS === 'web' ? 150 : 25,
+        }}>
             <Div className="footer-left" style={{ flexDirection: "row"}}>
                 <Text>Copyright &copy; 2018</Text><Div className="bullet"></Div>
                 <Text>Design By </Text><A href="https://nauval.in/">Muhamad Nauval Azhar</A>
