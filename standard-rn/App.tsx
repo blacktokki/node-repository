13cfdcc4ed8ca45cb8514e43c8c62bf8e0977cc7
blacktokki <39031723+blacktokki@users.noreply.github.com>
2021-06-19 00:22:37 +0900

    github pages2

--- standard-rn/App.tsx
+++ standard-rn/App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import _ from 'lodash';
+
+(function(l) {
+  if (l !== undefined && l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, '',
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
 
 const ignoreWarnings = ['ReactNativeFiberHostComponent'];
 const _console = _.clone(console);
